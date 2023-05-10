fa893cace7b3af1871913f7f3a07efe82fa16b11
Felizardo Massimbe <f2massimbe@gmail.com>
2023-05-10 13:07:02 +0100

    chore: setup redux
    install redux, reduxjs/toolkit and react-redux and put App in the provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
 
+const store = configureStore({ reducer: {} });
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
