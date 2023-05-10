ed4099ecb8f10d073cb1928871c6c835e6dbd993
Felizardo Massimbe <f2massimbe@gmail.com>
2023-05-10 14:15:28 +0100

    chore: setup styled components
    install styled-components and configure global theme

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 
 import "./index.css";
 import App from "./App";
+import { defaultTheme } from "./theme";
 
 const store = configureStore({ reducer: {} });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <ThemeProvider theme={defaultTheme}>
+          <App />
+        </ThemeProvider>
       </Router>
     </Provider>
   </React.StrictMode>
