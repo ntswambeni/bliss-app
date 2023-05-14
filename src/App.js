542a325c889ca2d8b8674bff9e6961716b3fc9e2
Felizardo Massimbe <f2massimbe@gmail.com>
2023-05-14 23:25:14 +0100

    fix: no connectivity screen is reseting state
    overlay do not no-connectivity component not remove the rendered screen
    
    disable scroll when there is no connectivity

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
 import { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router";
 import { connect } from "react-redux";
 
 import { handleSetServerStatus } from "./actions/serverStatus";
 import Loading from "./pages/loading/Loading";
 import Spinner from "./components/spinner";
-
-import { Navigate, Route, Routes } from "react-router";
 import QuestionList from "./pages/question-list/QuestionList";
 import QuestionDetails from "./pages/question-details/QuestionDetails";
 import NavOutlet from "./pages/nav-outlet/NavOutlet";
 import useNetwork from "./utils/useNetwork";
-import Cover from "./components/cover";
-import { Widget } from "./styled-components/Widget";
-import { Header1, Paragraph } from "./styled-components/Typography";
-import { withTheme } from "styled-components";
 import NotFound from "./pages/not-found/NotFound";
+import NoConnectivity from "./components/no-connectivity/NoConnectivity";
+import { createGlobalStyle } from "styled-components";
 
-function App({ dispatch, serverStatus, loading, theme }) {
+function App({ dispatch, serverStatus, loading }) {
   useEffect(() => {
     dispatch(handleSetServerStatus());
   }, [dispatch]);
 
   const isOnline = useNetwork();
-
-  if (!isOnline) {
-    return (
-      <Cover>
-        <Widget>
-          <Header1>
-            <span
-              className="material-symbols-outlined"
-              style={{ fontSize: "80px" }}
-            >
-              wifi_off
-            </span>
-          </Header1>
-          <Paragraph color={theme.colors.surface}>
-            Please check your internet connection.
-          </Paragraph>
-        </Widget>
-      </Cover>
-    );
-  }
 
   if (serverStatus !== "OK") {
     return (
@@ -52,8 +29,16 @@ function App({ dispatch, serverStatus, loading, theme }) {
     );
   }
 
+  const GlobalStyle = createGlobalStyle`
+  html, body { height: ${!isOnline && "100%"}; overflow: ${
+    !isOnline && "hidden"
+  }}
+ `;
+
   return (
     <>
+      {!isOnline && <NoConnectivity />}
+      <GlobalStyle />
       <Routes>
         <Route path="/" exact element={<Navigate to="/questions" />} />
         <Route path="/questions" element={<NavOutlet />}>
@@ -72,4 +57,4 @@ const mapStateToProps = ({ loading, serverStatus }) => ({
   serverStatus,
 });
 
-export default connect(mapStateToProps)(withTheme(App));
+export default connect(mapStateToProps)(App);
